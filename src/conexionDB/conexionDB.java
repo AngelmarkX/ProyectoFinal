47b5cefe2eb3074cd38b2f5d53c2598ccdd66a5c
Miguel Angel <136278328+AngelmarkX@users.noreply.github.com>
2023-11-14 15:23:12 -0500

    Cambio Carpetas

---
+++ src/conexionDB/conexionDB.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package conexionDB;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class conexionDB {
+    private static final String URL = "jdbc:mysql://localhost:3306/bibliotecadb";
+    private static final String USER = "root";
+    private static final String PASSWORD = "123456";
+
+   public static Connection getConnection() {
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(URL, USER, PASSWORD);
+            if (connection != null) {
+                System.out.println("Conexion exitosa a la base de datos.");
+            } else {
+                System.err.println("No se pudo establecer la conexión a la base de datos.");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return connection;
+    }
+}
